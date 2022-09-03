c4e81d2b745afdd34cdeb26a58fbe3e27a3437a9
SpacePython12 <andrewtwellman3@gmail.com>
2022-09-03 10:58:11 -0500

    varray implemented using macros!

--- include/varray.h
+++ include/varray.h
@@ -1,38 +1,78 @@
 #ifndef _VARRAY_H_
 #define _VARRAY_H_ 1
 
-#ifndef _STDLIB_H_ 
+#ifndef _STDLIB_H
 #include <stdlib.h>
 #endif
 
-typedef struct varray_t {
-    void * data;
-    size_t nblocks;
-    size_t blocksize;
-} varray_t;
-
-#ifdef __cplusplus
-extern "C" {
+#ifndef _STRING_H
+#include <string.h>
 #endif
 
-// Allocate a new varray. The caller is responsible for freeing this structure.
-varray_t * varray_new(size_t blocksize, size_t nblocks);
-// Allocate a new varray, and initialize it with DATA. The caller is responsible for freeing this structure.
-varray_t * varray_from(void * data, size_t blocksize, size_t nblocks);
-// Free this varray.
-void varray_free(varray_t * vl);
+// An abstract list of a given type.
+#define varray(T) struct varray_##T {T * data; size_t len;}
 
-// Returns the number of elements in the list.
-size_t varray_length(varray_t * vl);
-// Returns the element size of the list.
-size_t varray_elemsize(varray_t * vl);
-// Returns the raw data of the list.
-void * varray_data(varray_t * vl)
+// Macro containing the type size (as by sizeof()) of the given array.
+#define varray_tsize(arr) (sizeof(*(arr)->data))
+// Macro containing the raw pointer to the data. Use this only if you know what you are doing.
+#define varray_data(arr) ((arr)->data)
+// Macro containing the value at the given index in the given array.
+#define varray_at(arr, ind) ((arr)->data[ind])
+// Macro containing the length of the array. Don't write to this unless you know what you are doing.
+#define varray_len(arr) ((arr)->len)
 
-
-
-#ifdef __cplusplus
+// Initializes the given array with a valid data pointer and a given size.
+#define varray_init(arr, len)\
+{\
+    arr = malloc(sizeof(void *) + sizeof(size_t));\
+    varray_data(arr) = malloc(varray_tsize(arr) * len);\
+    varray_len(arr) = len;\
 }
-#endif
+// Initializes the given array with a valid data pointer and a given size, then copies data into it, without overflowing the allocated space.
+#define varray_initfrom(arr, data, len)\
+{\
+    varray_init(arr, len);\
+    memmove(varray_data(arr), data, varray_len(arr)*varray_tsize(arr));\
+}
+// Frees the given array.
+#define varray_free(arr)\
+{\
+    free(varray_data(arr));\
+    free(arr);\
+}
+// Resizes the given array to the given size.
+#define varray_resize(arr, len)\
+{\
+    varray_data(arr) = realloc(varray_data(arr), len*varray_tsize(arr));\
+    varray_len(arr) = len;\
+}
+// Inserts a given value at the given index.
+#define varray_insert(arr, ind, val)\
+{\
+    varray_resize(arr, varray_len(arr)+1);\
+    memmove(varray_data(arr)+(ind*varray_tsize(arr))+varray_tsize(arr), varray_data(arr)+(ind*varray_tsize(arr)), varray_len(arr)-ind);\
+    varray_at(arr, ind) = val;\
+}
+// Removes the value at the given index.
+#define varray_remove(arr, ind)\
+{\
+    memmove(varray_data(arr)+(ind*varray_tsize(arr))-varray_tsize(arr), varray_data(arr)+(ind*varray_tsize(arr)), varray_len(arr)-ind);\
+    varray_resize(arr, varray_len(arr)-1);\
+}
+// Sets all of the members of the given array to NULL or 0.
+#define varray_clear(arr)\
+{\
+    memset(varray_data(arr), 0, varray_len(arr)*varray_tsize(arr));\
+}
+// Sets the size of the given array to zero.
+#define varray_reset(arr)\
+{\
+    varray_resize(arr, 0);\
+}
+// Insert a value at the end of the array.
+#define varray_add(arr, val)\
+{\
+    varray_insert(arr, varray_len(arr), val);\
+}
 
 #endif
